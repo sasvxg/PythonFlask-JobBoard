8a305328dc741625e063cb789c486eae03d487ec
sasvxg <vino.gona@sas.com>
2023-05-03 15:51:55 -0400

    module1

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,10 @@
-# import Flask
-from Flask import render_template
+from flask import *
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@route('/jobs')
+
+@app.route('/')
+@app.route('/jobs')
 def jobs():
-    return(render_template('index.html'))
+    return render_template('index.html')
